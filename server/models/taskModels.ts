3a6621840f91f0c22df8db3604870b3a0329533d
James Huang <jhuang4647@gmail.com>
2024-03-19 17:51:01 -0400

    GET and POST requests are functional

--- server/models/taskModels.ts
+++ server/models/taskModels.ts
@@ -1,10 +1,25 @@
 import { Schema, model, connect } from 'mongoose';
 
+const MONGO_URI =
+  'mongodb+srv://jhuang4647:tkdnSiVrpDWDurmx@red.c3ytmlf.mongodb.net/';
+
+connect(MONGO_URI);
+
 interface Task {
-    title: string;
-    descrption: string;
-    dueDate: string;
-    createdAt: string;
-    createdBy: string;
+  title: string;
+  description: string;
+  dueDate: string;
+  createdBy: string;
 }
 
+const taskSchema = new Schema<Task>(
+  {
+    title: { type: String, required: true },
+    description: String,
+    dueDate: { type: String, required: true },
+    createdBy: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Task = model<Task>('Task', taskSchema);
